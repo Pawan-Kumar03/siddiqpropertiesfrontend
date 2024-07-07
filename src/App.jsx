beaeadbede94ba8c81d030565368121c09abe5af
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-07-07 13:20:24 +0500

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
-import router from "../../Server/routes/router";
+import router from "./routes/router";
 import { ListingsProvider } from "./contexts/ListingsContext";
 
 export default function App() {
