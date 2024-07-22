af76787e4efef6196b09ef56fcbc1e0a667f71e8
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-07-22 18:43:08 +0500

    Change Theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import { ListingsProvider } from "./contexts/ListingsContext";
-import './App.css'; 
+import './App.css'
 export default function App() {
     return (
         <ListingsProvider>
