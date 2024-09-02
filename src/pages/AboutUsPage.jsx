011ba6bb2238460ee6c2de4aaae476e51c4cfb48
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-09-02 23:49:52 +0500

    update about us page content

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,8 +1,26 @@
 export default function AboutUsPage() {
     return (
-        <div className="container mx-auto py-12 text-custom text-center  shadow-md border-4 border-custom">
-            <h1 className="text-3xl font-bold mb-6">About Us</h1>
-            <p>This is the About Us page. You can include information about your company here.</p>
+        <div className="container mx-auto py-12 text-custom   shadow-md border-4 border-custom">
+            <h1 className="text-3xl font-bold mb-6 text-center">About Us</h1>
+            <p>Welcome to <b>MASKAN</b>, your go-to platform for buying, selling, and renting properties. We connect property owners, real estate agents, and potential buyers or renters through a seamless and user-friendly experience.</p>
+
+<h1 className="text-3xl font-bold mb-6 text-center"> Who We Are</h1>
+<p>
+We are a team of real estate enthusiasts and tech professionals dedicated to making property transactions simple and accessible. Our mission is to empower you with the tools and information needed to make informed decisions in the real estate market.</p>
+
+<h1 className="text-3xl font-bold mb-6 text-center">What We Offer</h1>
+<p>
+Comprehensive Listings: Discover a wide range of properties, from residential to commercial.
+<h2 className="text-2xl font-bold mb-6">Advanced Search:</h2> Easily find properties that match your criteria with our robust filters.
+<h2 className="text-2xl font-bold mb-6">User-Friendly Interface:</h2> Navigate our platform effortlessly, whether you're a first-time user or a seasoned professional.
+<h2 className="text-2xl font-bold mb-6">Secure Transactions:</h2> Your privacy and security are our top priorities.
+<h2 className="text-2xl font-bold mb-6">Dedicated Support:</h2> Our team is always here to assist you with any questions or issues.
+<h1 className="text-3xl font-bold mb-6 text-center">Why Choose Us?</h1>
+<p>We provide a personalized, community-driven experience backed by cutting-edge technology. Whether you're searching for your dream home or listing a property, <b>MASKAN</b> is designed to meet your real estate needs.</p>
+
+Thank you for choosing <b>MASKAN</b>. We’re excited to be a part of your real estate journey!
+
+</p>
         </div>
     );
 }
