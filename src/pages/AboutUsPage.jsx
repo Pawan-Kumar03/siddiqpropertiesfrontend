9725a11c596d7356648768b553b560e5695bfb37
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-11-08 19:58:37 +0500

    update About Us

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -6,10 +6,18 @@ export default function AboutUsPage() {
 
 <h1 className="text-3xl font-bold mb-6 text-center"> Who We Are</h1>
 <p>
-Our Team
+We are a team of real estate enthusiasts and tech professionals dedicated to making property transactions simple and accessible. Our mission is to empower you with the tools and information needed to make informed decisions in the real estate market.</p>
+<h1 className="text-3xl font-bold mb-6 text-center">What We Offer</h1>
+<p>
+Comprehensive Listings: Discover a wide range of properties, from residential to commercial.</p>
+<h2 className="text-2xl font-bold mb-6">Advanced Search:</h2> Easily find properties that match your criteria with our robust filters.
+<h2 className="text-2xl font-bold mb-6">User-Friendly Interface:</h2> Navigate our platform effortlessly, whether you're a first-time user or a seasoned professional.
+<h2 className="text-2xl font-bold mb-6">Secure Transactions:</h2> Your privacy and security are our top priorities.
+<h2 className="text-2xl font-bold mb-6">Dedicated Support:</h2> Our team is led by Sami Siddiq, a seasoned expert with extensive knowledge in real estate, technology, and data analytics. Sami’s unique blend of expertise allows us to stay ahead of industry trends and leverage the latest tools to provide our clients with cutting-edge solutions. Under his leadership, we are committed to transforming the property transaction experience, making it more efficient, transparent, and accessible for everyone. Together, we aim to empower you with the insights and resources needed to succeed in today’s competitive real estate market.
+<h1 className="text-3xl font-bold mb-6 text-center">Why Choose Us?</h1>
+<p>We provide a personalized, community-driven experience backed by cutting-edge technology. Whether you're searching for your dream home or listing a property, <b>MASKAN</b> is designed to meet your real estate needs.</p>
+Thank you for choosing <b>MASKAN</b>. We’re excited to be a part of your real estate journey!
 
-Our team is led by Sami Siddiq, a seasoned expert with extensive knowledge in real estate, technology, and data analytics. Sami’s unique blend of expertise allows us to stay ahead of industry trends and leverage the latest tools to provide our clients with cutting-edge solutions. Under his leadership, we are committed to transforming the property transaction experience, making it more efficient, transparent, and accessible for everyone. Together, we aim to empower you with the insights and resources needed to succeed in today’s competitive real estate market.
-</p>
         </div>
     );
 }
