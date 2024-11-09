42622f517580d910d87100e36ea916d81136cf4c
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-11-09 23:38:46 +0500

    set navbar

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -37,67 +37,77 @@ export default function Navbar() {
     return (
         <header className="bg-gray-800">
             <div className="lg:border-b lg:border-b-gray-600">
-                <nav className="relative container mx-auto p-4 text-gray-100 flex items-center justify-between">
-                    {/* Logo on the top left */}
+                <nav className="container mx-auto p-4 text-gray-100 flex items-center justify-between">
+                    {/* Logo */}
                     <div className="flex items-center">
                         <Link to="/">
                             <img style={logoStyle} src={logo} alt="logo" />
                         </Link>
                     </div>
 
-                    {/* Navigation Links on the top right */}
-                    <div className="flex items-center space-x-6">
-                        <Link to="/" className="bg-custom text-black py-2 px-4 rounded cursor-pointer">Home</Link>
-                        <Link to="/about-us" className="bg-custom text-black py-2 px-4 rounded cursor-pointer">About Us</Link>
+                    {/* Hamburger Menu for Mobile */}
+                    <div className="block lg:hidden">
+                        <button 
+                            className="text-white focus:outline-none"
+                            onClick={() => setDropdownOpen(!dropdownOpen)}
+                        >
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                            </svg>
+                        </button>
+                    </div>
 
-                        {/* Login Button with Dropdown Menu */}
-                        {/* <div className="relative"> */}
-                            {user ? (
-                                <div className="relative">
-                                    <span
-                                        className="bg-custom text-black py-2 px-4 rounded cursor-pointer dropdown-toggle"
-                                        onClick={toggleDropdown}
-                                    >
-                                        {user.name} {/* Display username */}
-                                    </span>
-                                    {dropdownOpen && (
-                                        <div id="dropdown" className="absolute right-0 mt-2 bg-white text-black rounded shadow-lg w-48 z-50">
-                                            <Link
-                                                to="/profile"
-                                                className="block px-4 py-2 hover:bg-gray-200"
-                                                onClick={() => setDropdownOpen(false)}
-                                            >
-                                                My Profile
-                                            </Link>
-                                            <Link
-                                                to="/get-verified"
-                                                className="block px-4 py-2 hover:bg-gray-200"
-                                                onClick={() => setDropdownOpen(false)}
-                                            >
-                                                Get Verified
-                                            </Link>
-                                            <Link
-                                                to="/my-ads"
-                                                className="block px-4 py-2 hover:bg-gray-200"
-                                                onClick={() => setDropdownOpen(false)}
-                                            >
-                                                My Ads
-                                            </Link>
-                                            <button
-                                                onClick={handleLogout}
-                                                className="w-full text-left px-4 py-2 hover:bg-gray-200"
-                                            >
-                                                Sign Out
-                                            </button>
-                                        </div>
-                                    )}
-                                </div>
-                            ) : (
-                                <Link to="/login" className="bg-custom text-black py-2 px-4 rounded cursor-pointer">
-                                    Login
-                                </Link>
-                            )}
-                        {/* </div> */}
+                    {/* Main Navigation Links */}
+                    <div className={`lg:flex lg:items-center lg:space-x-6 ${dropdownOpen ? 'block' : 'hidden'} lg:block`}>
+                        <Link to="/" className="block text-white py-2 px-4 rounded lg:inline-block lg:bg-transparent lg:text-black bg-custom">Home</Link>
+                        <Link to="/about-us" className="block text-white py-2 px-4 rounded lg:inline-block lg:bg-transparent lg:text-black bg-custom">About Us</Link>
+
+                        {/* Dropdown Menu */}
+                        {user ? (
+                            <div className="relative">
+                                <button
+                                    className="block text-white py-2 px-4 rounded lg:inline-block lg:bg-transparent lg:text-black bg-custom cursor-pointer dropdown-toggle"
+                                    onClick={toggleDropdown}
+                                >
+                                    {user.name}
+                                </button>
+                                {dropdownOpen && (
+                                    <div id="dropdown" className="absolute right-0 mt-2 bg-white text-black rounded shadow-lg w-full lg:w-48 z-50">
+                                        <Link
+                                            to="/profile"
+                                            className="block px-4 py-2 hover:bg-gray-200"
+                                            onClick={() => setDropdownOpen(false)}
+                                        >
+                                            My Profile
+                                        </Link>
+                                        <Link
+                                            to="/get-verified"
+                                            className="block px-4 py-2 hover:bg-gray-200"
+                                            onClick={() => setDropdownOpen(false)}
+                                        >
+                                            Get Verified
+                                        </Link>
+                                        <Link
+                                            to="/my-ads"
+                                            className="block px-4 py-2 hover:bg-gray-200"
+                                            onClick={() => setDropdownOpen(false)}
+                                        >
+                                            My Ads
+                                        </Link>
+                                        <button
+                                            onClick={handleLogout}
+                                            className="w-full text-left px-4 py-2 hover:bg-gray-200"
+                                        >
+                                            Sign Out
+                                        </button>
+                                    </div>
+                                )}
+                            </div>
+                        ) : (
+                            <Link to="/login" className="block text-white py-2 px-4 rounded lg:inline-block lg:bg-transparent lg:text-black bg-custom">
+                                Login
+                            </Link>
+                        )}
                     </div>
                 </nav>
             </div>
