16a253632c55e7562e2e3d5f7bed9ca39696f313
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-11-09 15:55:01 +0500

    fix about us page

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,16 +1,16 @@
 export default function AboutUsPage() {
     return (
         <div className="container mx-auto py-12 text-white   shadow-md ">
-            <h1 className="text-3xl font-bold margin-top:1.5rem margin-bottom:1.5rem text-custom">About Us</h1>
+            <h1 className="text-3xl font-bold my-6 text-custom">About Us</h1>
             <p>Welcome to <b>MASKAN</b>, your go-to platform for buying, selling, and renting properties. We connect property owners, real estate agents, and potential buyers or renters through a seamless and user-friendly experience.</p>
 
-<h1 className="text-3xl font-bold mb-6 text-custom"> Who We Are</h1>
+<h1 className="text-3xl font-bold my-6 text-custom"> Who We Are</h1>
 <p>
 We are a team of real estate enthusiasts and tech professionals dedicated to making property transactions simple and accessible. Our mission is to empower you with the tools and information needed to make informed decisions in the real estate market.</p>
-<h1 className="text-3xl font-bold mb-6 text-custom">Our Team</h1>
+<h1 className="text-3xl font-bold my-6 text-custom">Our Team</h1>
 <p>
 Our team is led by Sami Siddiq, a seasoned expert with extensive knowledge in real estate, technology, and data analytics. Sami’s unique blend of expertise allows us to stay ahead of industry trends and leverage the latest tools to provide our clients with cutting-edge solutions. Under his leadership, we are committed to transforming the property transaction experience, making it more efficient, transparent, and accessible for everyone. Together, we aim to empower you with the insights and resources needed to succeed in today’s competitive real estate market.</p>
-<h1 className="text-3xl font-bold mb-6 text-custom">Why Choose Us?</h1>
+<h1 className="text-3xl font-bold my-6 text-custom">Why Choose Us?</h1>
 <p>We provide a personalized, community-driven experience backed by cutting-edge technology. Whether you're searching for your dream home or listing a property, <b>MASKAN</b> is designed to meet your real estate needs.</p>
 Thank you for choosing <b>MASKAN</b>. We’re excited to be a part of your real estate journey!
 
