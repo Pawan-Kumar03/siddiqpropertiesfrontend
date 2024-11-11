7ecd0f087215a485a34f51e88510be17377f892b
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-11-11 14:32:43 +0500

    update about us page

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -10,8 +10,10 @@ We are a team of real estate enthusiasts and tech professionals dedicated to mak
 <h1 className="text-3xl font-bold my-6 text-custom">Our Team</h1>
 <p>
 Our team is led by Sami Siddiq, a seasoned expert with extensive knowledge in real estate, technology, and data analytics. Sami’s unique blend of expertise allows us to stay ahead of industry trends and leverage the latest tools to provide our clients with cutting-edge solutions. Under his leadership, we are committed to transforming the property transaction experience, making it more efficient, transparent, and accessible for everyone. Together, we aim to empower you with the insights and resources needed to succeed in today’s competitive real estate market.</p>
-<h1 className="text-3xl font-bold my-6 text-custom">Why Choose Us?</h1>
-<p>We provide a personalized, community-driven experience backed by cutting-edge technology. Whether you're searching for your dream home or listing a property, <b>MASKAN</b> is designed to meet your real estate needs.</p>
+<h1 className="text-3xl font-bold my-6 text-custom">Why Maskan Consultancy?</h1>
+<p><b>Maskan</b> provides a bespoke, community driven experience backed by cutting edge technology. 
+Whether you are searching for your dream home, looking for an investment or simply listing a property. Maskan is here to meet your real estate needs. 
+</p>
 Thank you for choosing <b>MASKAN</b>. We’re excited to be a part of your real estate journey!
 
         </div>
