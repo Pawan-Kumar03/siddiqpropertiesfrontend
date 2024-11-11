fa66cbe932bc9a19db8233fc1f7340b3099186c9
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-11-11 14:56:00 +0500

    update banner img

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -136,13 +136,12 @@ export default function Banner({ onSearch, onPlaceAnAd }) {
     };
 
     return (
-        <section>
-<div className="container bg-cover bg-center bg-no-repeat lg:my-2 lg:pb-10 lg:pt-5 rounded-m bg-gray-800">
+        <section >
+<div className="container bg-cover bg-center bg-no-repeat lg:my-2 lg:pb-10 lg:pt-5 rounded-m bg-gray-800"
+style={{ backgroundImage: `url('/homepage-banner-img.jpeg')` }} 
+>
         {/* "Your Maskan Journey Starts Here" Section */}
-        <div
-        className="container bg-cover bg-center bg-no-repeat lg:my-2 lg:pb-10 lg:pt-5 rounded-m bg-gray-800"
-        style={{ backgroundImage: `url('/homepage-banner-img.jpeg')` }} 
-      ></div>
+
         <h1 className="text-2xl text-center font-semibold text-white lg:text-white lg:mb-8">
           Your Maskan Journey Starts Here
         </h1>
