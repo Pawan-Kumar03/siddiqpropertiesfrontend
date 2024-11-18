00b44be22ea677d77b807f02381c0651a30ba68c
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-11-19 00:51:25 +0500

    change display of property

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -24,7 +24,7 @@ export default function PropertyDetails() {
     } else {
       fetchProperty();
     }
-  }, [id, listings]); // Ensure it reacts to changes in listings
+  }, [id, listings]);
 
   const fetchProperty = async () => {
     try {
@@ -38,43 +38,6 @@ export default function PropertyDetails() {
       setProperty(data);
     } catch (error) {
       console.error("Failed to fetch property:", error);
-    }
-  };
-
-  const handleEditProperty = () => {
-    navigate(`/edit-property/${property._id}`);
-  };
-
-  const handleDeleteProperty = async () => {
-    const user = localStorage.getItem("user");
-    const parsedUser = JSON.parse(user);
-    const token = parsedUser.token;
-
-    try {
-      const response = await fetch(
-        `https://backend-git-main-pawan-togas-projects.vercel.app/api/listings/${property._id}`,
-        {
-          method: "DELETE",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message);
-      }
-
-      setIsDeleted(true);
-      // Optionally, you could also update the listings context here
-
-      setTimeout(() => {
-        navigate("/");
-      }, 2000);
-    } catch (error) {
-      console.error("Failed to delete listing:", error);
     }
   };
 
@@ -120,20 +83,13 @@ export default function PropertyDetails() {
   };
 
   return (
-    <div className="container mt-8 bg-gray-800 text-gray-100 p-4 rounded-lg">
-      {isDeleted && (
-        <div className="text-center bg-green-200 text-green-700 p-4 rounded mb-4">
-          Your ad has been deleted successfully!
-        </div>
-      )}
+    <div className="container mt-8 bg-gray-800 text-gray-100 p-6 rounded-lg">
       {!isDeleted && property && (
         <>
-          <h2 className="text-custom text-xl font-semibold mb-3">
-            Property Details
-          </h2>
+          <h2 className="text-2xl font-bold mb-4">{property.title}</h2>
           <div className="flex flex-col lg:flex-row">
-            <div className="lg:w-1/2 lg:pr-4">
-              {property.images && processImages(property.images).length > 1 ? (
+            <div className="lg:w-1/2 lg:pr-6">
+              {property.images && processImages(property.images).length > 0 ? (
                 <Carousel
                   showThumbs={false}
                   infiniteLoop
@@ -142,13 +98,10 @@ export default function PropertyDetails() {
                   className="h-80"
                 >
                   {processImages(property.images).map((image, index) => (
-                    <div
-                      key={index}
-                      className="h-100 flex justify-center items-center"
-                    >
+                    <div key={index}>
                       <img
                         className="rounded-lg object-cover h-80 w-full"
-                        src={`${image}`}
+                        src={image}
                         alt={property.title}
                       />
                     </div>
@@ -157,33 +110,41 @@ export default function PropertyDetails() {
               ) : (
                 <img
                   className="rounded-lg mb-4 object-cover h-80 w-full"
-                  src={`${property.image}`}
+                  src={property.image}
                   alt={property.title}
                 />
               )}
             </div>
-            <div className="lg:w-1/2 lg:pl-4">
-              <h3
-                className="text-lg font-semibold mb-2"
-                style={{ color: "#c5a47e" }}
-              >
-                {property.title}
+            <div className="lg:w-1/2 lg:pl-6">
+              <h3 className="text-lg font-semibold text-yellow-500 mb-2">
+                {property.price} AED
               </h3>
-              <p className="text-sm mb-2">{property.price}</p>
-              <p className="mb-4 text-sm">
-                {property.city}, {property.location}
+              <p className="text-sm mb-2">
+                <strong>City:</strong> {property.city}
               </p>
-              <p className="mb-4 text-sm">{property.propertyType}</p>
-              <p className="mb-4 text-sm">{property.beds} Beds</p>
-              <p className="mb-4 text-sm">{property.baths} Baths</p>
-              <p className="mb-4 text-sm">Landlord: {property.landlordName}</p>
-              <p className="mb-4 text-sm">
-                {property.status === "false"
-                  ? "Property Incomplete"
-                  : "Property Complete"}
+              <p className="text-sm mb-2">
+                <strong>Location:</strong> {property.location}
               </p>
-              <p className="mb-4 text-sm">Broker: {property.broker}</p>
-              <div className="mb-4 flex items-center space-x-4 text-gray-100">
+              <p className="text-sm mb-2">
+                <strong>Property Type:</strong> {property.propertyType}
+              </p>
+              <p className="text-sm mb-2">
+                <strong>Beds:</strong> {property.beds}
+              </p>
+              <p className="text-sm mb-2">
+                <strong>Baths:</strong> {property.baths}
+              </p>
+              <p className="text-sm mb-2">
+                <strong>Landlord:</strong> {property.landlordName}
+              </p>
+              <p className="text-sm mb-2">
+                <strong>Building:</strong> {property.building}
+              </p>
+              <p className="text-sm mb-2">
+                <strong>Status:</strong>{" "}
+                {property.status === "false" ? "Incomplete" : "Complete"}
+              </p>
+              <div className="mt-4 flex items-center space-x-4">
                 <EmailIcon
                   style={{ cursor: "pointer" }}
                   onClick={() => handleContactBroker("Email")}
@@ -197,67 +158,9 @@ export default function PropertyDetails() {
                   onClick={() => handleContactBroker("WhatsApp")}
                 />
               </div>
-
-              {user && property && user._id === property.user && (
-                <>
-                  <button
-                    onClick={handleEditProperty}
-                    className="px-6 py-3 bg-blue-600 bg-custom text-white rounded mr-2"
-                  >
-                    Edit Property
-                  </button>
-                  <button
-                    onClick={() => setShowDeleteModal(true)}
-                    className="px-6 py-3 bg-red-600 bg-custom text-white rounded"
-                  >
-                    Delete Property
-                  </button>
-                </>
-              )}
             </div>
           </div>
-
-          {/* Delete Confirmation Modal */}
-          {showDeleteModal && (
-            <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center">
-              <div className="bg-white rounded-lg p-8">
-                <h3 className="text-lg font-semibold mb-4 text-custom">
-                  Confirm Deletion
-                </h3>
-                <p className="mb-4 text-custom">
-                  Are you sure you want to delete this property?
-                </p>
-                <div className="flex justify-end space-x-4">
-                  <button
-                    onClick={() => setShowDeleteModal(false)}
-                    className="px-6 py-3 bg-gray-500 text-white rounded"
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    onClick={() => {
-                      handleDeleteProperty();
-                      setShowDeleteModal(false);
-                    }}
-                    className="px-6 py-3 bg-red-600 text-white rounded"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-            </div>
-          )}
         </>
-      )}
-      {isDeleted && (
-        <div className="flex justify-center mt-4">
-          <button
-            onClick={() => navigate("/")}
-            className="px-6 py-3 bg-green-600 text-white rounded"
-          >
-            Go to Home
-          </button>
-        </div>
       )}
     </div>
   );
