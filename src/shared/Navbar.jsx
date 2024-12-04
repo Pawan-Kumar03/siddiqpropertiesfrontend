75a75c4654613399c5ea63d582780ec36f07a3de
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-12-04 23:12:28 +0500

    update theme in navbar

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -40,7 +40,7 @@ export default function Navbar() {
     return (
 <header className="bg-white font-primary">
     <div className="lg:border-b lg:border-b-accent font-primary">
-        <nav className="relative font-primary container mx-auto p-4 flex items-center justify-between rounded-lg shadow-md bg-lightBlue max-w-5xl">
+        <nav className="relative font-primary container mx-auto p-4 flex items-center justify-between rounded-lg shadow-md bg-lightBlue">
             {/* Logo */}
             <div className="flex items-center">
                 <Link to="/">
