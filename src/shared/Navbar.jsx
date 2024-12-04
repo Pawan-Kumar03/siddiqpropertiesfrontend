25b0d79ad5f44c7ae56f3e5081f5267fff72d206
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-12-05 00:02:28 +0500

    navbar redesign

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -40,179 +40,179 @@ export default function Navbar() {
     
     return (
 <header className="bg-white font-primary">
-            <div className="lg:border-b lg:border-b-accent font-primary">
-                <nav className="relative font-primary container mx-auto p-4 flex items-center justify-between rounded-lg shadow-md" 
-                     style={{ backgroundColor: 'rgba(0, 123, 255, 0.8)' }} >
-                    {/* Logo */}
-                    <div className="flex items-center">
-                        <Link to="/">
-                            <img style={logoStyle} src={logo} alt="logo" />
-                        </Link>
-                    </div>
+    <div className="lg:border-b lg:border-b-accent font-primary">
+        <nav className="relative font-primary container mx-auto p-4 flex items-center justify-between rounded-lg shadow-md bg-lightBlue" >
+            {/* Logo */}
+            <div className="flex items-center">
+                <Link to="/">
+                    <img style={logoStyle} src={logo} alt="logo" />
+                </Link>
+            </div>
 
-                    {/* Desktop Navigation */}
-                    <div className="hidden sm:flex items-center space-x-4" style={{ height: '60px' }}>
+            {/* Desktop Navigation */}
+            <div className="hidden sm:flex items-center space-x-4" style={{ height: '60px' }}>
+                <Link
+                    to="/"
+                    className="bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 shadow-sm"
+                >
+                    Home
+                </Link>
+                <Link
+                    to="/about-us"
+                    className="bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 shadow-sm"
+                >
+                    About Us
+                </Link>
+                <Link
+                    to="/ConsultancyPage"
+                    className="bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 shadow-sm"
+                >
+                    Consultant
+                </Link>
+                {user ? (
+    <div className="relative">
+        <span
+            className="bg-white text-primary py-2 px-4 rounded-full cursor-pointer dropdown-toggle shadow-sm"
+            onClick={toggleDropdown}
+        >
+            {user.name}
+        </span>
+        {dropdownOpen && (
+            <div
+                id="dropdown"
+                className="absolute right-0 mt-2 bg-white text-black rounded-lg shadow-lg w-48 z-50"
+            >
+                <Link
+                    to="/profile"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                    onClick={() => setDropdownOpen(false)}
+                >
+                    My Profile
+                </Link>
+                <Link
+                    to="/get-verified"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                    onClick={() => setDropdownOpen(false)}
+                >
+                    Get Verified
+                </Link>
+                <Link
+                    to="/my-ads"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                    onClick={() => setDropdownOpen(false)}
+                >
+                    My Ads
+                </Link>
+                <button
+                    onClick={handleLogout}
+                    className="w-full text-left px-4 py-2 hover:bg-gray-100"
+                >
+                    Sign Out
+                </button>
+            </div>
+        )}
+    </div>
+) : (
+    <Link
+        to="/login"
+        className="bg-button text-button py-2 px-4 rounded-full hover:bg-button-hover shadow-sm"
+    >
+        Login
+    </Link>
+)}
+
+            </div>
+            {/* Mobile Menu */}
+            <div className="sm:hidden flex items-center">
+                <button
+                    onClick={toggleMobileMenu}
+                    className="text-primary focus:outline-none"
+                >
+                    <svg
+                        className="w-6 h-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4 6h16M4 12h16M4 18h16"
+                        />
+                    </svg>
+                </button>
+
+                {mobileMenuOpen && (
+                    <div className="absolute right-0 top-12 bg-lightBlue text-primary rounded-lg shadow-lg w-48 z-50">
                         <Link
                             to="/"
-                            className="bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 shadow-sm transition-opacity duration-300 ease-in-out opacity-90 hover:opacity-100"
+                            className="block bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2"
+                            onClick={() => setMobileMenuOpen(false)}
                         >
                             Home
                         </Link>
                         <Link
                             to="/about-us"
-                            className="bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 shadow-sm transition-opacity duration-300 ease-in-out opacity-90 hover:opacity-100"
+                            className="block bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2"
+                            onClick={() => setMobileMenuOpen(false)}
                         >
                             About Us
                         </Link>
                         <Link
                             to="/ConsultancyPage"
-                            className="bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 shadow-sm transition-opacity duration-300 ease-in-out opacity-90 hover:opacity-100"
+                            className="block bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2"
+                            onClick={() => setMobileMenuOpen(false)}
                         >
                             Consultant
                         </Link>
                         {user ? (
-                            <div className="relative">
-                                <span
-                                    className="bg-white text-primary py-2 px-4 rounded-full cursor-pointer dropdown-toggle shadow-sm transition-opacity duration-300 ease-in-out opacity-90 hover:opacity-100"
-                                    onClick={toggleDropdown}
-                                >
-                                    {user.name}
-                                </span>
-                                {dropdownOpen && (
-                                    <div
-                                        id="dropdown"
-                                        className="absolute right-0 mt-2 bg-white text-black rounded-lg shadow-lg w-48 z-50"
-                                    >
-                                        <Link
-                                            to="/profile"
-                                            className="block px-4 py-2 hover:bg-gray-100"
-                                            onClick={() => setDropdownOpen(false)}
-                                        >
-                                            My Profile
-                                        </Link>
-                                        <Link
-                                            to="/get-verified"
-                                            className="block px-4 py-2 hover:bg-gray-100"
-                                            onClick={() => setDropdownOpen(false)}
-                                        >
-                                            Get Verified
-                                        </Link>
-                                        <Link
-                                            to="/my-ads"
-                                            className="block px-4 py-2 hover:bg-gray-100"
-                                            onClick={() => setDropdownOpen(false)}
-                                        >
-                                            My Ads
-                                        </Link>
-                                        <button
-                                            onClick={handleLogout}
-                                            className="w-full text-left px-4 py-2 hover:bg-gray-100"
-                                        >
-                                            Sign Out
-                                        </button>
-                                    </div>
-                                )}
-                            </div>
-                        ) : (
-                            <Link
-                                to="/login"
-                                className="bg-button text-button py-2 px-4 rounded-full hover:bg-button-hover shadow-sm transition-opacity duration-300 ease-in-out opacity-90 hover:opacity-100"
-                            >
-                                Login
-                            </Link>
-                        )}
-                    </div>
+    <>
+        <Link
+            to="/profile"
+            className="block bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2"
+            onClick={() => setMobileMenuOpen(false)}
+        >
+            My Profile
+        </Link>
+        <Link
+            to="/get-verified"
+            className="block bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2"
+            onClick={() => setMobileMenuOpen(false)}
+        >
+            Get Verified
+        </Link>
+        <Link
+            to="/my-ads"
+            className="block bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2"
+            onClick={() => setMobileMenuOpen(false)}
+        >
+            My Ads
+        </Link>
+        <button
+            onClick={handleLogout}
+            className="w-full text-left bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2"
+        >
+            Sign Out
+        </button>
+    </>
+) : (
+    <Link
+        to="/login"
+        className="block bg-button text-button py-2 px-4 rounded-full hover:bg-button-hover mb-2"
+        onClick={() => setMobileMenuOpen(false)}
+    >
+        Login
+    </Link>
+)}
 
-                    {/* Mobile Menu */}
-                    <div className="sm:hidden flex items-center">
-                        <button
-                            onClick={toggleMobileMenu}
-                            className="text-primary focus:outline-none"
-                        >
-                            <svg
-                                className="w-6 h-6"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            </svg>
-                        </button>
-
-                        {mobileMenuOpen && (
-                            <div className="absolute right-0 top-12 bg-lightBlue text-primary rounded-lg shadow-lg w-48 z-50">
-                                <Link
-                                    to="/"
-                                    className="block bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2 opacity-90 hover:opacity-100"
-                                    onClick={() => setMobileMenuOpen(false)}
-                                >
-                                    Home
-                                </Link>
-                                <Link
-                                    to="/about-us"
-                                    className="block bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2 opacity-90 hover:opacity-100"
-                                    onClick={() => setMobileMenuOpen(false)}
-                                >
-                                    About Us
-                                </Link>
-                                <Link
-                                    to="/ConsultancyPage"
-                                    className="block bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2 opacity-90 hover:opacity-100"
-                                    onClick={() => setMobileMenuOpen(false)}
-                                >
-                                    Consultant
-                                </Link>
-                                {user ? (
-                                    <>
-                                        <Link
-                                            to="/profile"
-                                            className="block bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2 opacity-90 hover:opacity-100"
-                                            onClick={() => setMobileMenuOpen(false)}
-                                        >
-                                            My Profile
-                                        </Link>
-                                        <Link
-                                            to="/get-verified"
-                                            className="block bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2 opacity-90 hover:opacity-100"
-                                            onClick={() => setMobileMenuOpen(false)}
-                                        >
-                                            Get Verified
-                                        </Link>
-                                        <Link
-                                            to="/my-ads"
-                                            className="block bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2 opacity-90 hover:opacity-100"
-                                            onClick={() => setMobileMenuOpen(false)}
-                                        >
-                                            My Ads
-                                        </Link>
-                                        <button
-                                            onClick={handleLogout}
-                                            className="w-full text-left bg-white text-primary py-2 px-4 rounded-full hover:bg-gray-100 mb-2 opacity-90 hover:opacity-100"
-                                        >
-                                            Sign Out
-                                        </button>
-                                    </>
-                                ) : (
-                                    <Link
-                                        to="/login"
-                                        className="block bg-button text-button py-2 px-4 rounded-full hover:bg-button-hover mb-2 opacity-90 hover:opacity-100"
-                                        onClick={() => setMobileMenuOpen(false)}
-                                    >
-                                        Login
-                                    </Link>
-                                )}
-                            </div>
-                        )}
                     </div>
-                </nav>
+                )}
             </div>
-        </header>
+        </nav>
+    </div>
+</header>
 
     );
 }
