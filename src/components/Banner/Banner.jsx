78ded3bb56b78604c25cca5f5c93676a0e5c2978
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-12-05 13:24:35 +0500

    update terms and consition design

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -326,7 +326,7 @@ export default function Banner({ onSearch, onPlaceAnAd }) {
         </div>
     </div>
     <div className="flex items-center justify-between mt-4 w-full ">
-                            <button type="submit" className="bg-button text-primary flex items-center justify-center  w-1/2 px-6 py-2 rounded-full font-semibold mr-2 ">
+                            <button type="submit" className="bg-button text-white flex items-center justify-center  w-1/2 px-6 py-2 rounded-full font-semibold mr-2 ">
                                 <img src={inputSearch} alt="Search" className="w-5 h-5 mr-2 font-playfair" />
                                 Search
                             </button>
--- src/pages/TermsAndConditions.jsx
+++ src/pages/TermsAndConditions.jsx
@@ -14,42 +14,42 @@ const TermsAndConditions = () => {
     };
 
     return (
-        <div className="min-h-screen flex justify-center items-center bg-gray-800 font-playfair">
-            <div className="bg-grey-dark p-8 rounded shadow-md w-full max-w-md w-full max-w-md font-playfair bg-grey-darker p-8 rounded shadow-md border-4 border-custom">
-                <h2 className="text-2xl font-bold mb-6 text-white text-center">Terms and Conditions</h2>
-                <div className="text-white text-sm">
+        <div className="min-h-screen flex justify-center items-center bg-primary font-playfair">
+            <div className="bg-primary p-8 rounded shadow-md w-full max-w-md w-full max-w-md font-playfair bg-primary p-8 rounded shadow-md border-4 border-lightBlue">
+                <h2 className="text-2xl font-bold mb-6 text-primary text-center">Terms and Conditions</h2>
+                <div className="text-primary text-sm">
                     <p className="mb-4">
-<h3 className="text-1xl font-bold mb-6 text-white text-center">1. Introduction</h3>
+<h3 className="text-1xl font-bold mb-6 text-primary text-center">1. Introduction</h3>
 By using <b>InvestiBayt</b>, you agree to these Terms and Conditions. If you disagree, please do not use our website.
 
-<h3 className="text-1xl font-bold mb-6 text-white text-center">2. Account Responsibilities</h3>
+<h3 className="text-1xl font-bold mb-6 text-primary text-center">2. Account Responsibilities</h3>
 Eligibility: Must be 18 or older.
 Account Security: You are responsible for maintaining your account and password confidentiality.
-<h3 className="text-1xl font-bold mb-6 text-white text-center">3. Property Listings</h3>
+<h3 className="text-1xl font-bold mb-6 text-primary text-center">3. Property Listings</h3>
 Accuracy: Ensure your property information is correct and lawful.
 Disclaimers: We do not guarantee listing accuracy or availability. Transactions are between users.
-<h3 className="text-1xl font-bold mb-6 text-white text-center">4. Privacy and Security</h3>
+<h3 className="text-1xl font-bold mb-6 text-primary text-center">4. Privacy and Security</h3>
 Privacy Policy: Governed by our Privacy Policy.<br></br>
 Security: We strive to protect your data, but cannot ensure complete security.
-<h3 className="text-1xl font-bold mb-6 text-white text-center">5. Prohibited Actions</h3>
+<h3 className="text-1xl font-bold mb-6 text-primary text-center">5. Prohibited Actions</h3>
 No Misuse: Do not misuse the website or submit false information.<br></br>
 Compliance: Follow all applicable laws and do not use automated tools without permission.
-<h3 className="text-1xl font-bold mb-6 text-white text-center">6. Limitation of Liability</h3>
+<h3 className="text-1xl font-bold mb-6 text-primary text-center">6. Limitation of Liability</h3>
 We are not liable for any damages resulting from your use of the website.
 
-<h3 className="text-1xl font-bold mb-6 text-white text-center">7. Termination</h3>
+<h3 className="text-1xl font-bold mb-6 text-primary text-center">7. Termination</h3>
 We may suspend or terminate your account for violations of these terms.
 
-<h3 className="text-1xl font-bold mb-6 text-white text-center">8. Changes</h3>
+<h3 className="text-1xl font-bold mb-6 text-primary text-center">8. Changes</h3>
 We may update these terms periodically. Continued use implies acceptance of the changes.
 
-<h3 className="text-1xl font-bold mb-6 text-white text-center">9. Governing Law</h3>
+<h3 className="text-1xl font-bold mb-6 text-primary text-center">9. Governing Law</h3>
 These terms are governed by the laws of the United Arab Emirates. Disputes will be handled in the courts of Dubai, UAE.                    </p>
                 </div>
                 <div className="mt-6 text-center">
                     <button
                         onClick={handleBack}
-                        className="bg-custom text-black py-2 px-4 rounded transition-colors duration-300 hover:bg-custom-dark"
+                        className="bg-primary text-primary py-2 px-4 rounded transition-colors duration-300 hover:bg-primary"
                     >
                         Back
                     </button>
