124bfad1c4565e0552e77d877824059d2cc22ee1
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-12-07 02:15:37 +0500

    redesign app bg

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -3,7 +3,7 @@ import Navbar from "../shared/Navbar";
 import Footer from '../shared/Footer'
 export default function Main() {
     return (
-        <div className="bg-primary min-h-screen relative overflow-x-hidden">
+        <div className="bg-white/40 min-h-screen relative overflow-x-hidden">
             <Navbar />
             <Outlet />
             <Footer/>
