10dc5f25cd71cbd0a18c488484d5f5ba8de4caee
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-12-10 01:48:07 +0500

    add pdf feature

--- src/pages/PlaceAnAdPage.jsx
+++ src/pages/PlaceAnAdPage.jsx
@@ -197,7 +197,7 @@ export default function PlaceAnAdPage() {
     console.log(
       `Compressed PDF size: ${(compressedPdfBlob.size / 1024).toFixed(2)} KB`
     );
-      // Ensure the compressed file is below 100 KB
+      // Ensure the compressed file is below 2000 KB
       if (compressedPdfBlob.size > 2000 * 1024) {
         throw new Error("Compressed PDF exceeds 100 KB limit");
       }
