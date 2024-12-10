42310413f5c8eb95484ca2073717230f1c8770e2
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-12-10 21:35:10 +0500

    theme setup

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,28 +1,28 @@
 export default function AboutUsPage() {
     return (
-        <div className="flex font-primary items-center justify-center min-h-screen bg-[#fff7f1] px-4">
+        <div className="flex font-primary items-center justify-center min-h-screen bg-primary px-4">
             <div className="w-full max-w-3xl bg-accent-color p-8 rounded-lg shadow-lg">
-                <h1 className="text-4xl font-bold my-6 text-black text-center">About Us</h1>
-                <p className="text-black mb-6">
+                <h1 className="text-4xl font-bold my-6 text-primary text-center">About Us</h1>
+                <p className="text-primary mb-6">
                     Welcome to <b >InvestiBayt</b>, your go-to platform for buying, selling, and renting properties. We connect property owners, real estate agents, and potential buyers or renters through a seamless and user-friendly experience.
                 </p>
 
-                <h2 className="text-3xl text-black font-semibold my-6 ">Who We Are</h2>
-                <p className="text-black mb-6">
+                <h2 className="text-3xl text-primary font-semibold my-6 ">Who We Are</h2>
+                <p className="text-primary mb-6">
                     We are a team of real estate enthusiasts and tech professionals dedicated to making property transactions simple and accessible. Our mission is to empower you with the tools and information needed to make informed decisions in the real estate market.
                 </p>
 
-                <h2 className="text-3xl text-black font-semibold my-6 ">Our Team</h2>
-                <p className="text-black mb-6">
+                <h2 className="text-3xl text-primary font-semibold my-6 ">Our Team</h2>
+                <p className="text-primary mb-6">
                     Our team is led by Sami Siddiq, a seasoned expert with extensive knowledge in real estate, technology, and data analytics. Sami’s unique blend of expertise allows us to stay ahead of industry trends and leverage the latest tools to provide our clients with cutting-edge solutions. Under his leadership, we are committed to transforming the property transaction experience, making it more efficient, transparent, and accessible for everyone. Together, we aim to empower you with the insights and resources needed to succeed in today’s competitive real estate market.
                 </p>
 
-                <h2 className="text-3xl text-black font-semibold my-6 ">Why InvestiBayt Consultancy?</h2>
-                <p className="text-black mb-6">
+                <h2 className="text-3xl text-primary font-semibold my-6 ">Why InvestiBayt Consultancy?</h2>
+                <p className="text-primary mb-6">
                     <b >InvestiBayt</b> provides a bespoke, community-driven experience backed by cutting-edge technology. Whether you are searching for your dream home, looking for an investment, or simply listing a property, InvestiBayt is here to meet your real estate needs.
                 </p>
 
-                <p className="text-black font-semibold">
+                <p className="text-primary font-semibold">
                     Thank you for choosing <b >InvestiBayt</b>. We’re excited to be a part of your real estate journey!
                 </p>
             </div>
