a67fe6fe6dad91775d4202d9a4df4cdae7778d6d
pawankumar <pawan@Pawans-MacBook-Pro.local>
2024-12-16 23:02:03 +0500

    banner in one row

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -416,12 +416,14 @@ export default function Banner({ onSearch, onPlaceAnAd }) {
   </div>
 
   <div class="flex items-end w-[10%]">
+  <label class="mb-1 text-sm font-medium text-gray-700"></label>
     <button type="submit" class="w-full p-2 h-10 bg-button text-button rounded-md text-sm font-medium">
       Search
     </button>
   </div>
 
   <div class="flex items-end w-[10%]">
+  <label class="mb-1 text-sm font-medium text-gray-700"></label>
     <button type="submit" class="w-full p-2 h-10 bg-primary text-primary rounded-md text-sm font-medium">
       Clear
     </button>
