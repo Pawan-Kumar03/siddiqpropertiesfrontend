a9993c49bb488fbacdcc171e50671fe1387f2f42
pawankumar <pawan@Pawans-MacBook-Pro.local>
2025-04-10 03:39:59 +0500

    update About Us page

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -4,7 +4,7 @@ export default function AboutUsPage() {
             <div className="w-full max-w-3xl bg-accent-color p-8 rounded-lg shadow-lg">
                 <h1 className="text-4xl font-bold my-6 text-primary text-center">About Us</h1>
                 <p className="text-primary mb-6">
-                    Welcome to <b >SiddiqProperties</b>, your go-to platform for buying, selling, and renting properties. We connect property owners, real estate agents, and potential buyers or renters through a seamless and user-friendly experience.
+                    Welcome to <b >Siddiq Properties</b>, your go-to platform for buying, selling, and renting properties. We connect property owners, real estate agents, and potential buyers or renters through a seamless and user-friendly experience.
                 </p>
 
                 <h2 className="text-3xl text-primary font-semibold my-6 ">Who We Are</h2>
@@ -17,13 +17,13 @@ export default function AboutUsPage() {
                     Our team is led by Sami Siddiq, a seasoned expert with extensive knowledge in real estate, technology, and data analytics. Sami’s unique blend of expertise allows us to stay ahead of industry trends and leverage the latest tools to provide our clients with cutting-edge solutions. Under his leadership, we are committed to transforming the property transaction experience, making it more efficient, transparent, and accessible for everyone. Together, we aim to empower you with the insights and resources needed to succeed in today’s competitive real estate market.
                 </p>
 
-                <h2 className="text-3xl text-primary font-semibold my-6 ">Why Siddiqproperties Consultancy?</h2>
+                <h2 className="text-3xl text-primary font-semibold my-6 ">Why Siddiq Properties Consultancy?</h2>
                 <p className="text-primary mb-6">
-                    <b >Siddiqproperties</b> provides a bespoke, community-driven experience backed by cutting-edge technology. Whether you are searching for your dream home, looking for an investment, or simply listing a property, Siddiqproperties is here to meet your real estate needs.
+                    <b >Siddiq Properties</b> provides a bespoke, community-driven experience backed by cutting-edge technology. Whether you are searching for your dream home, looking for an investment, or simply listing a property, Siddiq Properties is here to meet your real estate needs.
                 </p>
 
                 <p className="text-primary font-semibold">
-                    Thank you for choosing <b >Siddiqproperties</b>. We’re excited to be a part of your real estate journey!
+                    Thank you for choosing <b >Siddiq Properties</b>. We’re excited to be a part of your real estate journey!
                 </p>
             </div>
         </div>
